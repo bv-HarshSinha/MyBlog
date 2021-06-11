5597bd8e1b81dc3fcf4d758e79bb5ac503b21294
All <harsh.sinha@blogvault.net>
2021-06-11 19:07:28 +0530

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to:'sessions#destroy'
 end
