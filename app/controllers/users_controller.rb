8e12224b4a39738b8e496e72f17ec491f143bc04
All <harsh.sinha@blogvault.net>
2021-06-13 15:18:46 +0530

    UI restriction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def show
     @articles = @user.articles
   end
@@ -35,6 +36,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -43,4 +51,12 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
+  def require_same_user
+    if current_user != @user && ! current_user.admin?
+      flash[:alert] = "Alert you can only edit or delete your own account"
+      redirect_to @user
+    end
+  end
+
 end
